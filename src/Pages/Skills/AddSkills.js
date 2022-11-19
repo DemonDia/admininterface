b9d7c051efa5754c6e18f5797462659425ae0f15
Setsuna Yukiii <siang-meng@live.com>
2022-11-20 00:27:27 +0800

    Add skill page

--- src/Pages/Skills/AddSkills.js
+++ src/Pages/Skills/AddSkills.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import SkillForm from "../../Components/Skills/SkillForm"
 
-function AddSkills(props) {
+function AddSkills() {
     return (
         <div>
             <h1>Add skills</h1>
-            
+            <Button>
+                <Link to = "/skills">Back</Link>
+            </Button>
+            <SkillForm skillId = {null}></SkillForm>
         </div>
     );
 }
