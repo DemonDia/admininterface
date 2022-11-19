4f599942d2839cf1c5136e2e1381c131e9572f63
Setsuna Yukiii <siang-meng@live.com>
2022-11-19 23:36:30 +0800

    adding main menu and its links

--- src/Pages/MainMenu.js
+++ src/Pages/MainMenu.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { Card } from "@mui/material";
 
 function MainMenu(props) {
     return (
         <div>
-            <h1>Main menu</h1>
-            
+            <Card
+                sx={{
+                    maxWidth: "80%",
+                    margin: "10px auto",
+                    padding: "10px",
+                    alignItems: "middle",
+                }}
+            >
+                <h1>Main Menu</h1>
+                <div style={{ display: "grid" }}>
+                    <Button variant="text">
+                        <Link to="/skills">Manage Skills</Link>
+                    </Button>
+                    <Button variant="text">
+                        <Link to="/projects">Manage Projects</Link>
+                    </Button>
+                    <Button variant="text">
+                        <Link to="/experience">Manage Experiences</Link>
+                    </Button>
+                    {/* <Button variant="text">Manage Contacts</Button> */}
+                </div>
+            </Card>
         </div>
     );
 }
