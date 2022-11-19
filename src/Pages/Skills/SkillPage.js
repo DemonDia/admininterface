c79f11cf26393daa9357c19e30d48d061685f532
Setsuna Yukiii <siang-meng@live.com>
2022-11-20 00:09:05 +0800

    get all skills

--- src/Pages/Skills/SkillPage.js
+++ src/Pages/Skills/SkillPage.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import env from "react-dotenv";
 
+import axios from "axios";
 function SkillPage(props) {
+    const baseURL = env.BACKEND_API;
+    const [skills, setSkills] = useState([]);
+    const getSkills = async () => {
+        await axios.get(baseURL + "/skills").then((res) => {
+            console.log(res);
+            if (res.data.success) {
+                setSkills(res.data.data);
+            }
+        });
+    };
+    useEffect(() => {
+        // console.log(env.BACKEND_API)
+        getSkills();
+    }, []);
     return (
         <div>
             <h1>Skill Page</h1>
