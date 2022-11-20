4ad39a8ea15a049cc8bff7c3cc853d131061ac45
Setsuna Yukiii <siang-meng@live.com>
2022-11-20 11:24:32 +0800

    add experience page

--- src/Pages/Experiences/AddExperience.js
+++ src/Pages/Experiences/AddExperience.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import ExperienceForm from "../../Components/Experiences/ExperienceForm"
 function AddExperience(props) {
     return (
         <div>
             <h1>Add Experience</h1>
+            <Button>
+                <Link to = "/experience">Back</Link>
+            </Button>
+            <ExperienceForm experience = {null}></ExperienceForm>
         </div>
     );
 }
