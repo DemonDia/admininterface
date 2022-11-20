bc57784f85bf211454b6dea04358e2877b63df82
Setsuna Yukiii <siang-meng@live.com>
2022-11-20 13:57:20 +0800

    adding project form page

--- src/Pages/Projects/AddProjects.js
+++ src/Pages/Projects/AddProjects.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import ProjectForm from "../../Components/Projects/ProjectForm"
 function AddProjects(props) {
     return (
         <div>
             <h1>Add projects</h1>
+            <Button>
+                <Link to = "/skills">Back</Link>
+            </Button>
+            <ProjectForm project = {null}></ProjectForm>
         </div>
     );
 }
