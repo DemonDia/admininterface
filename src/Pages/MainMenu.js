03ec1f58d5b2a8f6da7a079a7e3b7b455b43fc56
Setsuna Yukiii <siang-meng@live.com>
2022-11-21 17:30:53 +0800

    importing of file WIP

--- src/Pages/MainMenu.js
+++ src/Pages/MainMenu.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { Card } from "@mui/material";
 
+import axios from "axios";
 function MainMenu(props) {
+    const viewUploadedFile = (e) => {
+        const fileReader = new FileReader();
+        fileReader.readAsText(e.target.files[0], "UTF-8");
+        fileReader.onload = (e) => {
+            const jsonChunk = JSON.parse(e.target.result)
+            const experiences = jsonChunk.experience
+            const projects = jsonChunk.project
+            const skills = jsonChunk.skill
+
+            console.log(experiences)
+            console.log(projects)
+            console.log(skills)
+            // upload the experiences, project into json
+        };
+    };
+
     return (
         <div>
             <Card
@@ -24,6 +41,17 @@ function MainMenu(props) {
                     </Button>
                     <Button variant="text">
                         <Link to="/experience">Manage Experiences</Link>
+                    </Button>
+                    <Button variant="text" component="label">
+                        <input
+                            type="file"
+                            accept="json"
+                            onChange={(e) => viewUploadedFile(e)}
+                        />
+                    </Button>
+
+                    <Button variant="filled" component="label">
+                        Import
                     </Button>
                     {/* <Button variant="text">Manage Contacts</Button> */}
                 </div>
